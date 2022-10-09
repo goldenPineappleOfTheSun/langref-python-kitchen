6a1ca8997abce550db4462ee9cf6ed2a28920e8b
saetov <djcore@yandex.ru>
2022-10-10 03:00:20 +0500

    🍉. Сделай набор удобных методов работы с холодильником. Методы для покладывания предметов внутрь и для их извлечения по имени. Например, если программист после тебя напишет "взять из холодоса яблоко" то холодильник выдамт первое попавшееся

--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 class Chicken:
     def __init__(self):
-        pass
+        self.name = 'курица'
 
-    def __print__(self):
+    def __str__(self):
         return '🍗'
 
 class Apple:
     def __init__(self):
-        pass
+        self.name = 'яблоко'
 
-    def __print__(self):
+    def __str__(self):
         return '🍎'
 
 class Refrigerator:
     def __init__(self):
         self.items = []
+        self.capacity = 10
+
+    def add(self, product):
+        if (len(self.items) < self.capacity):
+            self.items.append(product)
+        return self
+
+    def get(self, name):
+        for item in self.items:
+            if item.name == name:
+                self.items.remove(item)
+                return item
+
+    def __str__(self):
+        return str([str(x) for x in self.items])
+
+f = Refrigerator()
+f.add(Apple())
+    .add(Apple())
+    .add(Chicken())
+print(f.get('яблоко'))
+print(f)
