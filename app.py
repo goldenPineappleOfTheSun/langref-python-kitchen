2a9aeaf7111a5cfd41ea972dad35914532b39908
saetov <djcore@yandex.ru>
2022-10-10 02:15:03 +0500

    🍓. Создать несколько объектов различных продуктов, каждый из них, когда его принтят рисует соответствующий емодзи

---
+++ app.py
@@ -0,0 +1,13 @@
+class Chicken:
+    def __init__(self):
+        pass
+
+    def __print__(self):
+        return '🍗'
+
+class Apple:
+    def __init__(self):
+        pass
+
+    def __print__(self):
+        return '🍎'
